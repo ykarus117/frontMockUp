0a5841cb516633cf06782f5f1cc5592bb9afb598
yka <halo159@hotmail.it>
2024-02-15 18:27:11 +0100

    Finished polishing main page, changed color palette

--- src/api/main.js
+++ src/api/main.js
@@ -6,11 +6,11 @@ async function fetchSingleUser(id){
         .then(data => {
             user = data;
         });
-    userInfoFields = '<h1 class="flex flex-row sticky place-content-between top-0 p-0.5 bg-slate-400/90 rounded w-full text-center border-b-2 border-dotted border-violet-500 font-bold">' +
+    userInfoFields = '<h1 class="flex flex-row sticky place-content-between top-0 p-0.5 border-2 border-dashed rounded-md bg-violet-300 w-full text-center  border-violet-500 font-bold">' +
         '         <!--Close button-->\n' +
-        '            <button class="rounded place-self-center bg-slate-700 hover:bg-purple-700/80 h-5 w-5 shadow-lg"' +
+        '            <button class="rounded-full place-self-center hover:outline hover:outline-2 hover:outline-slate-400/90 bg-purple-700/70 h-5 w-5"' +
         '                    @click="userView=false">\n' +
-        '              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="times"><path fill="currentColor" d="M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>\n' +
+        '              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="times"><path fill="white" d="M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>\n' +
         '            </button>\n' +
         '          <!--End close button-->'
         +
@@ -24,11 +24,24 @@ async function fetchSingleUser(id){
 function userInfoFill(currentObject){
     let userInfoHTML = '';
     for (let key in currentObject){
-        userInfoHTML += '<div>' + '<p class="w-full font-bold">' + key + ': </p>' ;
         if (typeof currentObject[key] === 'object' && currentObject[key] !== null ) {
-            userInfoHTML+= '<br>' + userInfoFill(currentObject[key]);
+            let uppedKey = key[0].toUpperCase() + key.slice(1);
+            userInfoHTML +=
+                `<button class="bg-violet-300 outline-yellow-600 outline-1 outline rounded-md border-violet-500 w-full text-center font-bold" 
+                  @click="$refs.${uppedKey}.classList.toggle('hidden')"
+                  >${uppedKey}</button>
+                  <div x-ref="${uppedKey}" class="bg-violet-200">`;
+            userInfoHTML += userInfoFill(currentObject[key]);
+            userInfoHTML += '</div>';
         }else {
-            userInfoHTML += '<p>' + currentObject[key] + '</p></div>';
+            userInfoHTML +=
+                '<div class="bg-violet-200">' +
+                '<p class="w-full text-center font-bold">' +
+                key +
+                ': </p>' +
+                '<p class="text-center">' +
+                currentObject[key] +
+                '</p></div>';
         }
     }
     return userInfoHTML;
@@ -38,7 +51,7 @@ async function tableUpdate(tableHTML){
     await fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(data => {
-            tableHTML = '<tr>' +
+            tableHTML = '<tr class="rounded-md bg-yellow-500">' +
                 '<th>ID</th>' +
                 '<th>Name</th>' +
                 '<th>Username</th>' +
@@ -51,7 +64,7 @@ async function tableUpdate(tableHTML){
             data.forEach(user => {
                 tableHTML += `<tr
                   x-on:click="selectedUser = fetchSingleUser(${user.id}), userView = true"
-                  class="border-t-2 hover:outline-2 hover:outline outline-violet-400 hover:rounded border-violet-400"
+                  class="outline-offset-0 odd:bg-violet-300 even:bg-violet-200 hover:outline-2 hover:outline hover:rounded outline-yellow-600"
                   >
                   <td>${user.id}</td>
                   <td>${user.name}</td>
